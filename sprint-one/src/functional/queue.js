3ff632267f6488cebcc36998764e6855b5d33e40
Maestro501 <chldbwls0114@hotmail.com>
2014-02-26 12:42:13 -0800

    - que specific bahaviour

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,27 @@ var makeQueue = function(){
   var storage = {};
 
   // Implement the methods below
+  var size = 0;
 
-  instance.enqueue = function(value){
+  instance.enqueue = function(value){ //this would potentially override the property
+    for (var key in storage){
+      storage[key+1] = storage[key];
+    }
+    storage[0]=value;
+    size++;
   };
 
   instance.dequeue = function(){
+    var item = storage[size-1]
+    if(size>0){        
+      delete storage[size-1];
+      size--;
+    };
+    return item;
   };
 
   instance.size = function(){
+    return size;
   };
 
   return instance;
