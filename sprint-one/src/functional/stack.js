908fa395b2c70d9a59f7abbbaf894a94fcf13ef5
Maestro501 <chldbwls0114@hotmail.com>
2014-02-26 12:42:48 -0800

    complete

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,25 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var size; // Hint: set an initial value here
+  var size=0; // Hint: set an initial value here
 
   // Implement the methods below
   instance.push = function(value){
+    storage[size]=value;
+    size++;
   };
 
   instance.pop = function(){
+    var item = storage[size-1];
+    if(item!=undefined){
+      delete storage[size-1];
+      size--;
+      return item;
+    };
   };
 
   instance.size = function(){
+    return size;
   };
 
   return instance;
