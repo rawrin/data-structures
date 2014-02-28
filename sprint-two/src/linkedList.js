a2289d8a7b6e04f82713869e069e16bdf69b0933
Hack Reactor Students <warrenng23@gmail.com>
2014-02-28 12:25:56 -0800

    Completed Linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,36 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    if(list.tail===null){
+      list.tail = makeNode(value);
+      list.head=list.tail;
+    }else{
+      list.tail.next = makeNode(value);
+      list.tail = list.tail.next;
+    }
   };
 
   list.removeHead = function(){
+    list.head = list.head.next;
   };
 
   list.contains = function(target, node){
+
+    var check = false;
+
+    var node = node||list.head;
+    while(check===false){
+      if(target===node.value){
+        check = true;
+      } 
+      if (node.next !== null) {
+        node = node.next;
+      } else {
+        check = node.value===target;
+        return check;
+      }
+    };
+    return check;
   };
 
   return list;
