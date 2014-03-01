b91efa7886cb18a504b62def72fed65a69466956
Hack Reactor Students <warrenng23@gmail.com>
2014-02-28 20:42:58 -0800

    Completed HashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,13 +4,22 @@ var HashTable = function(){
 };
 
 HashTable.prototype.insert = function(k, v){
+  //k = index, v = value in terms of set
   var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(i, v);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(i);
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.each(function(value, index, collection){
+    if(i===index){
+      collection[i]=null;
+    }
+  })
 };
 
