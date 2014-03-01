24fe13029110aad346805eede301a7263ac14b58
Hack Reactor Students <warrenng23@gmail.com>
2014-02-28 19:08:11 -0800

    completed set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,16 +1,28 @@
 var makeSet = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+   this._storage.push(item);
 };
 
 setPrototype.contains = function(item){
+  var check = false;
+  for(var i = 0; i < this._storage.length ; i ++ ) {
+    if(this._storage[i]===item){
+      return true;
+    }
+  };
+  return check;
 };
 
 setPrototype.remove = function(item){
+  var index = this._storage.indexOf(item);
+  if(index !== -1){
+    this._storage.splice(index, 1);
+  };
 };
