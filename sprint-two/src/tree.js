e164bff6c1295490f5ee16ee1773977a66ad9a39
Hack Reactor Students <warrenng23@gmail.com>
2014-02-28 16:24:33 -0800

    Tree mostly finished, contains has minor issue

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,16 +1,37 @@
 var makeTree = function(value){
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
-  newTree.children = undefined;
+  newTree.children = [];
+
+
   return newTree;
 };
+
+
 
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  var node = Object.create(treeMethods);
+  node.value= value;
+  node.children = [];
+  this.children.push(node);
 };
 
 treeMethods.contains = function(target){
+  var check = false;
+
+  for (var i = 0 ; i < this.children.length; i ++ ) {
+    console.log("Checking if: " + this.children[i].value + ", is equal to: " + target);
+      if (this.children[i].value===target) {
+        console.log("It's supposed to be true!");
+        return true;
+      }
+      this.children[i].contains(target);
+  };
+  console.log("Nothing is equal :[");
+  return check;
 };
+
 
