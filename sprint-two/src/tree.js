250a745a1bf2885f05ecefa2a9533c54a9b2d422
Hack Reactor Students <warrenng23@gmail.com>
2014-02-28 19:11:04 -0800

    Need to refactor old code for contains

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -14,24 +14,37 @@ var treeMethods = {};
 
 treeMethods.addChild = function(value){
   var node = Object.create(treeMethods);
-  node.value= value;
+  node.value = value;
   node.children = [];
   this.children.push(node);
 };
 
 treeMethods.contains = function(target){
-  var check = false;
+  
+  //check if initial value ===target;
+  //if value is === target return true
+  //if its not, do the same check for its children;
+  //if any children contain the value, return true
+  //else return false;
 
-  for (var i = 0 ; i < this.children.length; i ++ ) {
-    console.log("Checking if: " + this.children[i].value + ", is equal to: " + target);
-      if (this.children[i].value===target) {
-        console.log("It's supposed to be true!");
-        return true;
-      }
-      this.children[i].contains(target);
-  };
-  console.log("Nothing is equal :[");
-  return check;
+
+
+
+  // var check = false || this.value===target;
+
+
+
+  // for (var i = 0 ; i < this.children.length; i ++ ) {
+  //   console.log("Checking if: " + this.children[i].value + ", is equal to: " + target);
+  //     if (!!this.value===target) {
+  //       console.log("It's supposed to be true!");
+  //       return true;
+  //     }
+  //     return this.children[i].contains(target);
+  // };
+  // console.log("Nothing is equal :[");
+  // return check;
 };
+
 
 
